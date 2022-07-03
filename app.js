a9a713a4011ca25d3916b01e2b1ff8bc2194ed0a
ayanbajo <bolatitoayanbajo@gmail.com>
2022-07-03 10:47:19 -0400

    updated

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
 class Casino {
     // Write code here
+    constructor(name, fakeCoin) {
+        this.name = name;
+        this.timesPlayed = 0;
+        this.fakeCoin = fakeCoin;
+    }
+    updateToken(token) {
+        this.fakeCoin = token
+    }
+
+    playGame(betAmount) {
+        this.timesPlayed +=1
+        if (Math.random() <= 0.5 || !this.fakeCoin) {
+        console.log(`${this.name} wins`)
+    } else {
+        const winAmount = betAmount * (this.timesPlayed + 1)
+        console.log (`You Won ${winAmount} !`);
+    }
+   
+    }
 };
 
 // TESTS
-const myCasino = new Casino("HackerU Casino");
+const myCasino = new Casino("HackerU Casino", false);
 console.log(myCasino);
+myCasino.updateToken(false);
 myCasino.playGame(5);
 myCasino.playGame(15);
+myCasino.updateToken(true);
 myCasino.playGame(25);
 myCasino.playGame(35);
 
@@ -28,3 +49,9 @@ myExtraBonusCasino.rollDie(6);
 myExtraBonusCasino.rollDie(20);
 myExtraBonusCasino.rollDie(100);
 */
+
+// if (this.isFakeCoin = true) 
+//     console.log(`${this.name} wins`)
+//     else (
+//     console.log (`You Won`)
+// )
